8dcf054271155607d38f246216ae0de1f6b709a8
INOXBEAT <jpreciado85@gmail.com>
2024-04-03 08:56:37 -0500

    Encriptación de password del usuario

--- app/controllers/usuarios/create.php
+++ app/controllers/usuarios/create.php
@@ -7,14 +7,19 @@ $email = $_POST['email'];
 $password_user = $_POST['password_user'];
 $password_repeat = $_POST['password_repeat'];
 
-$sentencia = $pdo->prepare("INSERT INTO tb_usuarios 
+if ($password_user == $password_repeat) {
+    $password_repeat = password_hash($password_user, PASSWORD_DEFAULT);
+    $sentencia = $pdo->prepare("INSERT INTO tb_usuarios 
             (nombre, email, password_user, fyh_creacion) 
     VALUES  (:nombre, :email, :password_user, :fyh_creacion)");
 
-$sentencia->bindParam('nombre',$nombres);
-$sentencia->bindParam('email',$email);
-$sentencia->bindParam('password_user',$password_user);
-$sentencia->bindParam('fyh_creacion', $fecha_hora);
-$sentencia->execute();
+    $sentencia->bindParam('nombre', $nombres);
+    $sentencia->bindParam('email', $email);
+    $sentencia->bindParam('password_user', $password_user);
+    $sentencia->bindParam('fyh_creacion', $fecha_hora);
+    $sentencia->execute();
+} else {
+    echo "error las contraseñas no son iguales";
+}
 
 ?>
